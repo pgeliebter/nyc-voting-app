19afbefafaff48fef34a15d226b83043aedfae25
Pinny Geliebter <pgibralter@gmail.com>
2021-06-21 12:04:42 -0400

    adds votes file and cleans up voting_script.rb

--- lib/tasks/voting_script.rb
+++ lib/tasks/voting_script.rb
@@ -1,7 +1,5 @@
+load "votes.rb"
+
 candidates = { 1 => "Eric Adams", 2 => "Shaun Donovan", 3 => "Kathryn Garcia", 4 => "Raymond McGuire", 5 => "Dianne Morales", 6 => "Scott Stringer", 7 => "Maya Wiley", 8 => "Andrew Yang", 9 => "Art Chang", 10 => "Aaron Foldenauer", 11 => "Paperboy Love Prince", 12 => "Joycelyn Taylor", 13 => "Isaac Wright Jr." }
 
-votes = [[1, 5, 4, 9, 10]]
-
-p candidates[votes[0][0]]
-p candidates[0]
-p candidates[4]
+p @votes.length
